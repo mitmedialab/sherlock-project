cdb5623ae62e56a58794d05540bbaf5438d1a1d8
Chris Lowe <chris.lowe.uk@gmail.com>
2020-12-27 06:51:17 +0000

    moved CHARACTERS_TO_CHECK into helpers, cast double results to float (reduce data comms in IPC and multiprocessing and tensorflow uses float32)

--- sherlock/features/bag_of_characters.py
+++ sherlock/features/bag_of_characters.py
@@ -1,27 +1,12 @@
-import string
-import numpy  as np
+import numpy as np
 import pandas as pd
 from scipy.stats import skew, kurtosis
 from collections import OrderedDict
 from re import search
+from sherlock.features.helpers import CHARACTERS_TO_CHECK
+
 
 ZERO_SERIES = pd.Series([0], name='Empty')
-
-CHARACTERS_TO_CHECK = (
-        ['[' + c + ']' for c in string.printable if c not in ('\n', '\\', '\v', '\r', '\t', '^', '[', ']')]
-        + ['[\\\\]', '[\\^]', '[\\[]', '[\\]]']
-)
-
-ZERO_FLOAT64 = np.float64(0)
-DEFAULT_KURTOSIS_FLOAT64 = np.float64(-3.0)
-
-
-def extract_bag_of_characters_features(series: pd.Series):
-    features = OrderedDict()
-
-    extract_bag_of_characters_features(series, features)
-
-    return features
 
 
 # Input: a single column in the form of pandas series
@@ -48,22 +33,22 @@ def extract_bag_of_characters_features(series: pd.Series, features: OrderedDict)
         if has_any:
             features[value_feature_name + '-agg-any'] = has_any
             features[value_feature_name + '-agg-all'] = all(value_features)
-            features[value_feature_name + '-agg-mean'] = np.mean(value_features)
-            features[value_feature_name + '-agg-var'] = np.var(value_features)
+            features[value_feature_name + '-agg-mean'] = float(np.mean(value_features))
+            features[value_feature_name + '-agg-var'] = float(np.var(value_features))
             features[value_feature_name + '-agg-min'] = np.min(value_features)
             features[value_feature_name + '-agg-max'] = np.max(value_features)
             features[value_feature_name + '-agg-median'] = np.median(value_features)
             features[value_feature_name + '-agg-sum'] = np.sum(value_features)
-            features[value_feature_name + '-agg-kurtosis'] = kurtosis(value_features)
-            features[value_feature_name + '-agg-skewness'] = skew(value_features)
+            features[value_feature_name + '-agg-kurtosis'] = float(kurtosis(value_features))
+            features[value_feature_name + '-agg-skewness'] = float(skew(value_features))
         else:
             features[value_feature_name + '-agg-any'] = False
             features[value_feature_name + '-agg-all'] = False
-            features[value_feature_name + '-agg-mean'] = ZERO_FLOAT64
-            features[value_feature_name + '-agg-var'] = ZERO_FLOAT64
+            features[value_feature_name + '-agg-mean'] = 0.0
+            features[value_feature_name + '-agg-var'] = 0.0
             features[value_feature_name + '-agg-min'] = 0
             features[value_feature_name + '-agg-max'] = 0
             features[value_feature_name + '-agg-median'] = 0
             features[value_feature_name + '-agg-sum'] = 0
-            features[value_feature_name + '-agg-kurtosis'] = DEFAULT_KURTOSIS_FLOAT64
-            features[value_feature_name + '-agg-skewness'] = ZERO_FLOAT64
+            features[value_feature_name + '-agg-kurtosis'] = -3.0
+            features[value_feature_name + '-agg-skewness'] = 0.0
