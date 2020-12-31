a2c22cfb278e70c3414a15c1be81ee18d3f271a5
Chris Lowe <chris.lowe.uk@gmail.com>
2020-12-31 12:15:00 +0000

    added NLTK preprocessing for tokens passed into Doc2Vec; optimised paragraph vectors feature extraction

--- sherlock/features/paragraph_vectors.py
+++ sherlock/features/paragraph_vectors.py
@@ -3,7 +3,8 @@ import multiprocessing
 import gensim.models.doc2vec
 from collections import OrderedDict
 import pandas as pd
-import numpy as np
+import nltk
+from nltk.corpus import stopwords
 
 from gensim.models.doc2vec import Doc2Vec, TaggedDocument
 from datetime import datetime
@@ -11,16 +12,39 @@ from datetime import datetime
 assert gensim.models.doc2vec.FAST_VERSION > -1, "This will be painfully slow otherwise"
 
 
+def tokenise(values):
+    tokens = []
+
+    joined = ' '.join(s for s in values if len(s) >= 2).lower()
+    filtered = ''.join(e for e in joined if e.isalnum() or e.isspace())
+
+    for word in nltk.word_tokenize(filtered):
+        if len(word) < 2 or word in STOPWORDS_ENGLISH:
+            continue
+
+        tokens.append(word)
+
+    return tokens
+
+
 # Input: a collection of columns stored in a dataframe column 'values'
 # Output: tagged columns.
 # Only needed for training.
-def tagcol_paragraph_embeddings_features(train_data):
-    # Expects a dataframe with a 'values' column
-    train_data_values = train_data['values']
-
+def tagcol_paragraph_embeddings_features(train_data: pd.Series, train_labels: list):
     random.seed(13)
-    columns = [TaggedDocument(random.sample(col, min(1000, len(col))), [i]) for i, col in
-               enumerate(train_data_values.values)]
+
+    columns = []
+
+    for i, col in enumerate(train_data):
+        label = train_labels[i]
+        values = random.sample(col, min(1000, len(col)))
+
+        if len(values) > 0:
+            values = list(map(lambda s: '' if s is None else str(s), values))
+
+        tokens = tokenise(values)
+
+        columns.append(TaggedDocument(tokens, label))
 
     return columns
 
@@ -39,24 +63,47 @@ def train_paragraph_embeddings_features(columns, dim):
     model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
 
 
-model = None
+DIM = 400
+model: Doc2Vec = None
 
 
 def initialise_pretrained_model(dim):
     start = datetime.now()
     global model
 
-    filename = f'../sherlock/features/par_vec_trained_{dim}.pkl'
+    filename = f'../sherlock/features/par_vec_retrained_{dim}.pkl'
+
+    assert dim == DIM
 
     model = Doc2Vec.load(filename)
     model.delete_temporary_training_data(keep_doctags_vectors=True, keep_inference=True)
     print(f'Initialise Doc2Vec Model, {dim} dim, process took {datetime.now() - start} seconds. (filename = {filename})')
 
 
+STOPWORDS_ENGLISH = None
+
+
+def initialise_nltk():
+    start = datetime.now()
+
+    nltk.download('punkt')
+    nltk.download('stopwords')
+
+    global STOPWORDS_ENGLISH
+
+    STOPWORDS_ENGLISH = stopwords.words('english')
+
+    print(f'Initialised NLTK, process took {datetime.now() - start} seconds.')
+
+
+KEYS = (
+        [f'par_vec_{i}' for i in range(DIM)]
+)
+
+
 # Input: a single column in the form of a pandas Series.
 # Output: ordered dictionary holding paragraph vector features
 def infer_paragraph_embeddings_features(series: pd.Series, features: OrderedDict, dim, reuse_model):
-    global model
 
     if not reuse_model or model is None:
         # Load pretrained paragraph vector model
@@ -67,8 +114,10 @@ def infer_paragraph_embeddings_features(series: pd.Series, features: OrderedDict
     # https://github.com/RaRe-Technologies/gensim/issues/447
     model.random.seed(13)
 
-    # Infer paragraph vector for data sample
-    inferred = model.infer_vector(series, steps=20, alpha=0.025)
+    tokens = tokenise(series)
 
-    for idx, v in enumerate(inferred):
-        features['par_vec_' + str(idx)] = v
+    # Infer paragraph vector for data sample.
+    inferred = model.infer_vector(tokens, steps=20, alpha=0.025)
+
+    for idx, value in enumerate(inferred):
+        features[KEYS[idx]] = value
