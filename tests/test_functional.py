8fc9bb2ba9c79b0946accabeed20e635f059ba8d
Chris Lowe <chris.lowe.uk@gmail.com>
2020-12-31 12:13:35 +0000

    renamed test for functional code

--- tests/test_functional.py
+++ tests/test_functional.py
@@ -1,5 +1,7 @@
 import unittest
 from sherlock.functional import normalise_float
+import pandas as pd
+
 
 class FunctionalTestCase(unittest.TestCase):
     def test_normalise_float_remove_imprecision(self):
@@ -11,6 +13,14 @@ class FunctionalTestCase(unittest.TestCase):
         result = normalise_float(0.0000001)
 
         self.assertEqual('1e-07', result)
+
+    def test_handle_none(self):
+        series = pd.Series(['$', None, 'Aab$', '$$ca', 'C$B$', 'cat', '-0.12'])
+
+        series = series.apply(lambda s: '' if s is None else str(s))
+
+        print(len(series.iloc[1]))
+        print(series.iloc[1] == 'None')
 
 
 if __name__ == '__main__':
