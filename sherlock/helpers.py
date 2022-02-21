f43b0ae88f89d0d32015efeefd9be80bc7a5087e
Madelon Hulsebos <madelon@uvavpn-byodm-145-18-162-85.vpn.uva.nl>
2022-02-21 18:52:01 +0100

    Check if the zip file is present, to avoid confusion with symlink dir.

--- sherlock/helpers.py
+++ sherlock/helpers.py
@@ -9,16 +9,17 @@ def download_data():
     The data is downloaded from Google Drive and stored in the 'data/' directory.
     """
     data_dir = "../data/"
+    zip_filepath = "../data/data.zip"
     print(f"Downloading the raw data into {data_dir}.")
 
-    if not os.path.exists("../data/data/"):
+    if not os.path.exists(zip_filepath):
         print("Downloading data directory.")
         gdown.download(
             url="https://drive.google.com/uc?id=1-g0zbKFAXz7zKZc0Dnh74uDBpZCv4YqU",
             output=data_dir,
         )
 
-        with zipfile.ZipFile("../data/data.zip", "r") as zf:
+        with zipfile.ZipFile(zip_filepath, "r") as zf:
             zf.extractall(data_dir)
 
     print("Data was downloaded.")
