481c9a1219e014bfda35d8267b76377ff2b603c5
Madelon Hulsebos <madelon@uvavpn-byodm-145-18-164-15.vpn.uva.nl>
2022-02-22 11:44:13 +0100

    Change data download, remove symlink dir.

--- sherlock/helpers.py
+++ sherlock/helpers.py
@@ -8,18 +8,18 @@ def download_data():
     """Download raw and preprocessed data files.
     The data is downloaded from Google Drive and stored in the 'data/' directory.
     """
-    data_dir = "../data/"
+    data_dir = "../data/data/"
     zip_filepath = "../data/data.zip"
     print(f"Downloading the raw data into {data_dir}.")
 
-    if not os.path.exists(zip_filepath):
+    if not os.path.exists(data_dir):
         print("Downloading data directory.")
         gdown.download(
             url="https://drive.google.com/uc?id=1-g0zbKFAXz7zKZc0Dnh74uDBpZCv4YqU",
-            output=data_dir,
+            output=zip_filepath,
         )
 
         with zipfile.ZipFile(zip_filepath, "r") as zf:
-            zf.extractall(data_dir)
+            zf.extractall("../data/")
 
     print("Data was downloaded.")
